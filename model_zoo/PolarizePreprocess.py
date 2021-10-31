5754c4b27bddf3616833283f9b6d50ae854de3d6
Michael Huang <michaelyhuang23@gmail.com>
2021-10-31 16:45:00 -0400

    updated architecture for polarize prep

--- model_zoo/PolarizePreprocess.py
+++ model_zoo/PolarizePreprocess.py
@@ -3,17 +3,20 @@ from torch import nn
 
 class PolarizePreprocess(nn.Module):
     def __init__(self):
-        # input is of shape (12,9,3*8)
-        # we should upsample that to (128,96,2) or (128,96,1)
+        # video shape is 360 by 640 (W by H)
+        # input is of shape (9,16,3*8) maybe another larger shape (18,32,3*8)
+        # we should upsample that to (360,640,2)
         super(PolarizePreprocess, self).__init__()
         self.conv1 = nn.Conv2d(24, 72, (3,3), padding='same') # gather info of neighbor cells
-        self.upconv1 = nn.ConvTranspose2d(72, 36, (7,9), stride=3, padding=0)
-        self.upconv2 = nn.ConvTranspose2d(36, 36, (8,7), stride=3, padding=1) # this should scale up to 128, 96
+        self.upconv1 = nn.ConvTranspose2d(72, 36, (4,3), stride=2, padding=0)
+        self.upconv2 = nn.ConvTranspose2d(36, 36, (6,5), stride=3, padding=0) 
+        self.upconv3 = nn.ConvTranspose2d(36, 36, (16,12), stride=6, padding=0) 
         # set up depthwise if needed
         self.conv2 = nn.Conv2d(36, 128, (1,1)) # blow up channels for classification
         self.conv3 = nn.Conv2d(128, 2, (1,1)) # final classifier
         self.relu = nn.ReLU()
         self.tanh = nn.Tanh()
+        # maybe a good idea to use scale up module and back connections
     
     def save(self, file_name="PolarizePreprocess.weights"):
         torch.save(self.state_dict(), file_name)
@@ -25,7 +28,15 @@ class PolarizePreprocess(nn.Module):
         x = self.relu(self.conv1(x))
         x = self.relu(self.upconv1(x))
         x = self.relu(self.upconv2(x))
+        x = self.relu(self.upconv3(x))
         x = self.relu(self.conv2(x))
         x = self.tanh(self.conv3(x)) # try with tanh and relu and sigmoid
         return x	
 
+model = PolarizePreprocess()
+
+model.eval()
+
+a = torch.zeros((1,24,16,9))
+p = model(a)
+print(p.shape)
