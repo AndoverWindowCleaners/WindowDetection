7bb06f299c65f87988219e5ccf9431a61f493e75
Michael Huang <michaelyhuang23@gmail.com>
2021-11-29 23:10:36 -0500

    update parsing code for boxes and spec

--- preprocessing/generate_coco_file.py
+++ preprocessing/generate_coco_file.py
@@ -27,6 +27,23 @@ for lab_folder in os.listdir(lab_root):
 	vid = [pool_img(frame, 9, 16) for frame in vid]
 	vid = np.stack(vid, axis=0)
 	freqs, times, spectr = signal.spectrogram(vid, fs=30.0, window=('hamming'), noverlap=13, nperseg=14, axis=0, mode='magnitude') 
-    # all_frame has shape (T,9,16,3) (T)--how many frames are in the video
-    # output (N, 9, 16, 3, T') -- applies fourier analysis to each all_shape[start:end,i,j,k] --> (N)
-    # output (N, 9, 16, 3, T')
+	# all_frame has shape (T,16,9,3) (T)--how many frames are in the video
+	# output (N, 16, 9, 3, T') -- applies fourier analysis to each all_shape[start:end,i,j,k] --> (N)
+	# output (N, 16, 9, 3, T')
+	assert(len(spectr) == 8) 
+	spectr = np.transpose(spectr, (4,1,2,3,0)) # (T',16,9,3,8)
+	spectr = np.reshape(spectr, (spectr.shape[0], spectr.shape[1], spectr.shape[2], spectr.shape[3]*spectr.shape[4]))
+	labs_files = os.listdir(os.path.join(lab_root, lab_folder))
+	labs_files = [file for file in labs_files if '.txt' in file]
+	labs_files.sort()
+	for i,file in enumerate(labs_files):
+		perc = i/len(labs_files)
+		vidi = round(perc * len(vid))
+		speci = round(perc * len(spectr))
+		with open(os.path.join(lab_root,lab_folder,file),'r') as f:
+			oboxes = f.readlines()
+		boxes = []
+		for box in oboxes:
+			boxes.append([box[1]-box[3]/2, box[2]-box[4]/2, box[1]+box[3]/2, box[2]+box[4]/2])
+		
+
